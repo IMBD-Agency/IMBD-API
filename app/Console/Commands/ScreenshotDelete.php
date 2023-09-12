6f4efd679f4fea7a27017ecfe1959341f639868e
Rowshon Habib Rony <rhrony0009@gmail.com>
2023-09-12 13:31:25 +0600

    bug fix

--- app/Console/Commands/ScreenshotDelete.php
+++ app/Console/Commands/ScreenshotDelete.php
@@ -5,7 +5,6 @@ namespace App\Console\Commands;
 use App\Models\Screenshot;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Log;
 
 class ScreenshotDelete extends Command {
     /**
@@ -36,6 +35,5 @@ class ScreenshotDelete extends Command {
                 echo 'Deleted ' . $screenshot->image . '\n';
             }
         }
-        Log::info('Delete Done.');
     }
 }
