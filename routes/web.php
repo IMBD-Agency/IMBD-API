f3d3553adf635d19b5fe26bc877e56930172b416
Rowshon Habib Rony <rhrony0009@gmail.com>
2024-01-28 18:12:04 +0600

    fix

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 });
 
 Route::get('login', function () {
-    return redirect('https://www.imbdagency.com');
+    return redirect('https://www.imbdagency.com')->name('login');
 });
 
 Route::post('login', function () {
