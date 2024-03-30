4fa1b11af672627c34f3dc0712cde1ee26dbf1c7
Rowshon Habib Rony <rhrony0009@gmail.com>
2024-03-30 11:26:24 +0600

    log login data

--- app/Http/Controllers/API/V1/AuthController.php
+++ app/Http/Controllers/API/V1/AuthController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers\API\V1;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\API\V1\LoginRequest;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class AuthController extends Controller {
     public function login(LoginRequest $request) {
-
         if (Auth::attempt($request->only(['email', 'password']))) {
             $user = auth()->user();
+            Log::info($user->name . ' loged in.');
             return response()->json([
                 'status' => 'success',
                 'data' => [
@@ -19,6 +20,7 @@ class AuthController extends Controller {
                 ]
             ], 200);
         } else {
+            Log::info('Login failed for ' . $request->email);
             return response()->json([
                 'status' => 'error',
                 'message' => 'Crediantials not matched.',
@@ -28,6 +30,7 @@ class AuthController extends Controller {
 
     public function logout() {
         Auth::user()->currentAccessToken()->delete();
+        Log::info(auth()->user()->name . ' trying to logout.');
         return response()->json([
             'status' => 'success',
             'message' => 'Logout successfull.',
